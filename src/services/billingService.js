890398b9f749092648ec6af79d7bab12d53e8f23
Joydeep Maiti <joydeep127@gmail.com>
2021-04-12 20:24:32 +0530

    billing service added

---
+++ src/services/billingService.js
@@ -0,0 +1,40 @@
+import http from "./httpService";
+
+async function getBillByBookingId(id) {
+  try {
+    const { data: rooms } = await http.get(`${http.baseUrl}/billing/${id}`);
+    return rooms;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addBilling(data) {
+  try {
+    const response = await http.post(
+      `${http.baseUrl}/billing`,
+      data
+    );
+    return response
+  } catch (error) {
+    console.log(error);
+    return false
+  }
+}
+
+// async function updatePos(data) {
+//   try {
+//     const response = await http.patch(
+//       `${http.baseUrl}/pos`,
+//       data
+//     );
+//     if(response.status === 200)
+//       return true
+//     return false
+//   } catch (error) {
+//     console.log(error);
+//     return false
+//   }
+// }
+
+export default { getBillByBookingId,addBilling };

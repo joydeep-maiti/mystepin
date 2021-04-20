abf419053a0f0dfb6f866d5a54483091bf6e039d
navya.kankanala <navya.kankanala@wipro.com>
2021-04-20 22:04:13 +0530

    Adding Tax

--- src/components/Taxes/Taxes.jsx
+++ src/components/Taxes/Taxes.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import taxService from "../../services/taxService";
-
 import Loader from "../../common/Loader/Loader";
 import {
   DialogActions,
@@ -8,8 +7,45 @@ import {
   DialogTitle,
   Typography,
   Button,
-  makeStyles
+  makeStyles,
+  withStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  Input
+  
 } from "@material-ui/core";
+import { grey ,black,blue} from "@material-ui/core/colors";
+import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
+import ReplayOutlinedIcon from '@material-ui/icons/ReplayOutlined';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor:"#0088bc",
+    color: theme.palette.common.white,
+  },
+  body:{
+    fontWeight:"bold"
+  }
+  
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+
+
 
 const useStyles = makeStyles(theme => ({
   formGroup: {
@@ -31,6 +67,8 @@ const useStyles = makeStyles(theme => ({
 const Taxes = ({ onClose }) => {
   const classes = useStyles();
   const [taxSlabs, setTaxSlabs] = useState([]);
+  const [sam, setsam] = useState([]);
+  const [tax , setTax]=useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -40,14 +78,72 @@ const Taxes = ({ onClose }) => {
 
   const fetchData = async () => {
     const taxSlabs = await taxService.getTaxSlabs();
+    
     setTaxSlabs(taxSlabs);
     setLoading(false);
   };
+   
+  const handleInputChange = (e,i) => {
+    const slab = [...taxSlabs]
+    slab[i].lessThanAndEqual = Number(e.target.value)
+    slab[i+1].greaterThan = Number(e.target.value) +1
+    setTaxSlabs(slab)
+  }
+
+  const handlePercentInputChange = (e,i) => {
+    const slab = [...taxSlabs]
+    slab[i].taxPercent = Number(e.target.value)
+    setTaxSlabs(slab)
+  }
+
+
+  const handleEdit = (row,i) => {
+    console.log("taxEdit",row, i) 
+  setTax(row,i)
+   
+  }
+
+  const handleUndo = () => {
+    setTax({})
+  }
+
+  const handleUpdate = async () => {
+
+    const promises = []
+    taxSlabs.forEach(el=>{
+      promises.push(taxService.updatetaxDetails(el))
+    })
+    Promise.all(promises)
+    .then(res=>{
+      console.log("promiseAll res", res)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+   // setLoading(true);
+     
+    // if(taxSlabs[1].greaterThan >taxSlabs.lessThanAndEqual){
+     // const res = await taxService.updatetaxDetails(tax);
+    // }else{
+    //   alert("invalid");
+    // }
+   
+    //setLoading(false);
+   // if((res) && (taxSlabs[1].greaterThan < taxSlabs.lessThanAndEqual) ){
+     
+      
+    // }
+    // else{
+    //   alert("invalid");
+    // }
+    //console.log(taxSlabs[1].greaterThan)
+  }
 
   return (
     <React.Fragment>
-      <DialogTitle>Tax Slabs</DialogTitle>
+      {/* <DialogTitle>Tax Slabs</DialogTitle>
       {loading && <Loader color="#0088bc" />}
+      <table>
       <DialogContent>
         {taxSlabs.map(taxInfo => (
           <div key={taxInfo._id} className={classes.formGroup}>
@@ -57,7 +153,7 @@ const Taxes = ({ onClose }) => {
               className={classes.inputItems}
             >
               Greater Than{" "}
-              {<span className={classes.span}>{taxInfo.greaterThan}</span>}{" "}
+              {<span className={classes.span}>{taxInfo.greaterThan }</span>}{" "}
               {taxInfo.lessThanAndEqual ? "and less than equal to " : ""}
               {taxInfo.lessThanAndEqual && (
                 <span className={classes.span}>{taxInfo.lessThanAndEqual}</span>
@@ -67,12 +163,108 @@ const Taxes = ({ onClose }) => {
             <Typography>{`${taxInfo.taxPercent}%`}</Typography>
           </div>
         ))}
+        
       </DialogContent>
+
+      </table> */}
       {/* <DialogActions>
         <Button onClick={onClose} color="secondary">
           Close
         </Button>
       </DialogActions> */}
+       <DialogTitle>Tax Caluclation</DialogTitle>
+       
+    <TableContainer component={Paper}>
+    <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+           
+           
+          </TableRow>
+          <TableRow>
+             <StyledTableCell rowSpan={2} align="center">TAX</StyledTableCell>
+                        <StyledTableCell rowSpan={2} align="center">SETTINGS</StyledTableCell>
+                        
+                        
+                        <StyledTableCell colSpan={2} align="center" >PRICE RANGE</StyledTableCell>
+                        <StyledTableCell rowSpan={2} align="center">VALUE</StyledTableCell>
+                      
+                      </TableRow>
+                      <TableRow>                       
+                        <StyledTableCell align="center">RATE FROM</StyledTableCell>
+                        <StyledTableCell align="center">RATE TO</StyledTableCell>
+                       
+                      </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+          <StyledTableCell   align="center" rowSpan={4}><TextField   variant="outlined" value="GST"  disabled="true" ></TextField></StyledTableCell>
+              <StyledTableCell align="center"rowSpan={4} ><TextField   variant="outlined" value="%" disabled="true"></TextField></StyledTableCell>
+              
+          </TableRow>
+        {taxSlabs.map((row,i) => (
+            <TableRow  key={row._id} >
+              
+             {/* <StyledTableCell align="center"  ><TextField   variant="outlined" value={row.greaterThan } onChange={handleInputChange}  > </TextField></StyledTableCell> */}
+            
+            
+            {tax._id !== row._id && <StyledTableCell align="center"> <TextField  variant="outlined"  disabled="true" value={row.greaterThan} /></StyledTableCell>}
+                  {tax._id === row._id && <StyledTableCell align="center">
+                    <TextField  variant="outlined" type="number" label="greaterThan" name="greaterThan" required="true"  disabled="true" value={tax.greaterThan }  />
+                  </StyledTableCell>}
+           
+                 
+                  
+            
+            
+              {/* <StyledTableCell align="center" ><TextField   variant="outlined" value={row.lessThanAndEqual } ></TextField></StyledTableCell> */}
+
+              {/* {tax._id !== row._id && <StyledTableCell align="center"> <TextField  variant="outlined"  value={row.lessThanAndEqual } /></StyledTableCell>} */}
+                  <StyledTableCell align="center">
+                    <TextField   variant="outlined"  type="number" label="lessThanAndEqual" name="lessThanAndEqual" value={row.lessThanAndEqual }    onChange={(e)=>handleInputChange(e,i)} ></TextField>
+                  </StyledTableCell>
+
+              {/* <StyledTableCell align="center" ><TextField   variant="outlined" value={row.taxPercent}></TextField></StyledTableCell> */}
+
+
+              {/* {tax._id !== row._id && <StyledTableCell align="center"><TextField  variant="outlined"  value={row.taxPercent } /></StyledTableCell>} */}
+                  <StyledTableCell align="center">
+                    <TextField   variant="outlined" type="number"  label="taxPercent" name="taxPercent" value={row.taxPercent } onChange={(e)=>handlePercentInputChange(e,i)} ></TextField>
+                  </StyledTableCell>   
+
+                 
+
+               {/* {tax._id !== row._id && <TableCell align="center"><EditOutlinedIcon style={{cursor:"pointer"}} onClick={()=>handleEdit(row,i)}/></TableCell>}
+                  {tax._id === row._id && <TableCell align="center">
+                  <ReplayOutlinedIcon style={{cursor:"pointer"}} onClick={handleUndo}/>
+                    <SaveOutlinedIcon style={{cursor:"pointer"}} onClick={()=>handleUpdate,i}/>
+                  </TableCell>}   */}
+            
+                  </TableRow>
+              
+          ))}
+        </TableBody>
+        {/* <div style={{position:"relative",right:"-1070px",backgroundColor:"#0088bc",color:'white'}}>
+        <Button  variant="contained"  onClick={handleUpdate}>Save</Button>
+        </div> */}
+
+          <Button 
+            type="submit" 
+            variant="contained"
+            style={{backgroundColor:"#0088bc",color:'white',position:"relative",right:"-1300px"}} onClick={handleUpdate}> SAVE </Button>
+
+        {/* <Button 
+            type="submit" 
+            variant="contained"
+            style={{backgroundColor:"#0088bc",color:'white',position:"relative",right:"-1000px"}} onClick={handleUndo}> RESTORE </Button>
+                 */}
+      </Table>
+    </TableContainer>
+   
+
+
+
+   
     </React.Fragment>
   );
 };

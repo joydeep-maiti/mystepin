c2a7c59f63a85de5083cf21a24f8f5b68da3a2fd
Hariharan Sankar <ha20182680@wipro.com>
2021-05-13 12:25:52 +0530

    Added PrintBill

--- src/services/printBill.js
+++ src/services/printBill.js
@@ -2,7 +2,7 @@ import http from "./httpService";
 
 async function getAllCheckOuts() {
   try {
-    const { data: bills } = await http.get(`http://localhost:5000/getAllBills`);
+    const { data: bills } = await http.get(`${http.baseUrl}/getAllBills`);
     return bills;
   } catch (error) {
     console.log(error);
@@ -11,12 +11,12 @@ async function getAllCheckOuts() {
 //`${http.baseUrl}/getAllBills`
 async function getBillsByDate(date) {
   try {
-    const { data: bills } = await http.get(`http://localhost:5000/getBillsbyDate?date=${date}`);
+    const { data: bills } = await http.get(`${http.baseUrl}/getBillsbyDate?date=${date}`);
     return bills;
   } catch (error) {
     console.log(error);
   }
 }
-// `${http.baseUrl}/billing?date=${date}`
+// `http://localhost:5000/getBillsbyDate?date=${date}`
 export default { getAllCheckOuts,getBillsByDate};
 
